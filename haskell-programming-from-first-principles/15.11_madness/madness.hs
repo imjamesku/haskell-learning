ea8fd630f77695565e0a848fb475ab74577c5617
James Ku <jameskutw@gmail.com>
2024-03-03 11:22:42 +0800

    Add madness excercise

---
+++ haskell-programming-from-first-principles/15.11_madness/madness.hs
@@ -0,0 +1,20 @@
+type Verb = String
+
+type Adjective = String
+
+type Adverb = String
+
+type Noun = String
+
+type Exclamation = String
+
+madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbin' e adv noun adj = e <> "! he said " <> adv <> " as he jumped into his car " <> noun <> " and drove off with his " <> adj <> " wife."
+
+madlibbinBetter' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter' e adv noun adj = mconcat [e, "! he said ", adv, " as he jumped into his car ", noun, " and drove off with his ", adj, " wife."]
+
+main :: IO ()
+main = do
+  print $ madlibbin' "Wow" "quickly" "dog" "green"
+  print $ madlibbinBetter' "Wow" "quickly" "dog" "green"
