c982d0098f83142cb20606856ad8794d6a224ea7
James Ku <jameskutw@gmail.com>
2024-03-11 23:31:27 +0800

    Add multi-layer project

--- backprop-learning/app/Main.hs
+++ backprop-learning/app/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
+import Control.Lens (makeLenses)
+import GHC.Generics (Generic)
 import Lib
+import Numeric.Backprop (Backprop, sequenceVar)
+import Numeric.LinearAlgebra.Static (L, R)
 
 main :: IO ()
 main = someFunc
+
+data Net = N
+  { _nWeights1 :: L 20 100,
+    _nBias1 :: R 20,
+    _nWeights2 :: L 5 20,
+    _nBias2 :: R 5
+  }
+  deriving (Show, Generic)
+
+instance Backprop Net
+
+makeLenses ''Net
